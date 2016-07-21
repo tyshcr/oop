6739bd146af203aba8ad8a205aea06d3ea83f540
Christopher Tysh <chris@feltpad.net>
2016-07-21 11:17:15 -0500

    Initial Commit

---
+++ oop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oop
+//
+//  Created by Christopher Tysh on 7/21/16.
+//  Copyright © 2016 Christopher Tysh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ oop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  oop
+//
+//  Created by Christopher Tysh on 7/21/16.
+//  Copyright © 2016 Christopher Tysh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
