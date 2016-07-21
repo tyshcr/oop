c28e2986f47e6cc3c23dae5224f28e256f02498a
Christopher Tysh <chris@feltpad.net>
2016-07-21 12:39:24 -0500

    New unit tests

--- oopTests/oopTests.m
+++ oopTests/oopTests.m
@@ -7,6 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Person.h"
+#import "Wallet.h"
+#import "CreditCard.h"
 
 @interface oopTests : XCTestCase
 
@@ -24,16 +27,59 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testPerson {
+    Person *person = [[Person alloc] init];
+    person.firstname = @"Christopher";
+    person.lastname = @"Tysh";
+    XCTAssertEqualObjects([person fullname], @"Christopher Tysh", "Names are not equal");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testInterestValue {
+    CreditCard *card1 = [[CreditCard alloc] init];
+    card1.name = @"Visa";
+    card1.balance = 100.00;
+    card1.interestRate = 1.0;
+    
+    XCTAssertEqual([card1 interestEarned], 1.00, "Card 1 interest earned is incorrect");
+    XCTAssertEqual([card1 balanceAfterInterest], 101.00, "Card 1 new balance is incorrect");
+    
+    CreditCard *card2 = [[CreditCard alloc] init];
+    card2.name = @"MasterCard";
+    card2.balance = 500.00;
+    card2.interestRate = 2.0;
+    
+    XCTAssertEqual([card2 interestEarned], 10.00, "Card 2 interest earned is incorrect");
+    XCTAssertEqual([card2 balanceAfterInterest], 510.00, "Card 2 new balance is incorrect");
+}
+
+- (void)testWalletInterest {
+    CreditCard *card1 = [[CreditCard alloc] init];
+    card1.name = @"Visa";
+    card1.balance = 100.00;
+    card1.interestRate = 1.0;
+    
+    CreditCard *card2 = [[CreditCard alloc] init];
+    card2.name = @"MasterCard";
+    card2.balance = 500.00;
+    card2.interestRate = 2.0;
+    
+    Wallet *wallet = [[Wallet alloc] init];
+    wallet.name = @"My Wallet";
+    wallet.creditCards = [@[card1,card2] mutableCopy];
+    
+    XCTAssertEqual([wallet interestEarned], 11.00, "Wallet 1 interest earned is incorrect");
+    
+    CreditCard *card3 = [[CreditCard alloc] init];
+    card3.name = @"Discover";
+    card3.balance = 1000.00;
+    card3.interestRate = 10.0; // $100 interest
+    
+    [wallet.creditCards addObject:card3];
+    
+    card1.interestRate = 4.0; // $4 interest
+    card2.interestRate = 5.0; // $25 interest
+    
+    XCTAssertEqual([wallet interestEarned], 129.00, "Wallet 2 interest earned is incorrect");
 }
 
 @end
